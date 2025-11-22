74a4363a4a28d38b18b35ac3a47c6f80880402cf
Ayush <ayushak880@gmail.com>
2025-11-22 15:23:14 +0530

    creat post api to add school

--- backend/controller/school.controller.js
+++ backend/controller/school.controller.js
@@ -13,4 +13,35 @@ const getSchool = async (req, res) => {
   }
 };
 
-export { getSchool };
+const addSchool = async (req, res) => {
+  try {
+    const { schoolName, address } = req.body;
+    if (!schoolName || !address) {
+      return res.json({
+        message: "Fill required field.",
+      });
+    }
+    const [result] = await pool.query(
+      "INSERT INTO school (school_name, address) VALUES (?, ?)",
+      [schoolName, address]
+    );
+    return res.json({
+      success: true,
+      message: " Data Saved",
+      id: result.insertId,
+    });
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      return res.status(409).json({
+        success: false,
+        message: "School name already exists",
+      });
+    }
+    res.status(400).json({
+      success: false,
+      message: "Error to add school data",
+      Error: error,
+    });
+  }
+};
+export { getSchool, addSchool };
--- backend/routes/school.route.js
+++ backend/routes/school.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getSchool } from "../controller/school.controller.js";
+import { addSchool, getSchool } from "../controller/school.controller.js";
 
 const schoolRouter = express.Router();
 
 schoolRouter.get("/get", getSchool);
+schoolRouter.post("/add", addSchool);
 
 export default schoolRouter
